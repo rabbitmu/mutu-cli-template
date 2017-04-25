c7807430d434d2fee05597ce999bd0f43afd0d1b
muming <muming90@foxmail.com>
2017-04-25 16:35:00 +0800

    create throttle function

--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,7 @@
 import store from 'store/store';
 import { mapState } from 'vuex';
 import Example from 'components/Example';
+import throttle from 'utils/throttle';
 
 export default {
 	name: 'App',
@@ -10,9 +11,15 @@ export default {
 	}),
 	render() {
 		const { name } = this;
+		const style = {
+			overflowY: 'scroll',
+			maxHeight: '100%',
+			textAlign: 'center',
+			fontSize: '46px'
+		};
 
 		return (
-			<div style={ { textAlign: 'center', fontSize: '46px' } }>
+			<div style={ style }>
 				<Example name={ name } />
 			</div>
 		);
---
+++ src/utils/throttle.js
@@ -0,0 +1,11 @@
+export default function({ action, interval }) {
+	let timer;
+
+	return function() {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			console.log(timer);
+			action();
+		}, interval || 50);
+	};
+};
