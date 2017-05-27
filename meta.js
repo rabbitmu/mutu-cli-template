9d3acd1506473149ac5749760252d2f61a8f2857
mutu <rabbitmu@users.noreply.github.com>
2017-05-27 17:05:49 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -16,5 +16,5 @@ module.exports = {
             "message": "Author"
         }
     },
-    "completeMessage": "Download Sucess"
+    "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/rabbitmu/vue-boilerplate"
 }
