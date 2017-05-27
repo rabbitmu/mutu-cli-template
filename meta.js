051263a9d8d13cb5adf4abc09af447559acca908
muming <muming@daojia.com>
2017-05-27 15:37:27 +0800

    update meta.js

--- meta.js
+++ meta.js
@@ -28,5 +28,5 @@ module.exports = {
         "src/router/*": "router",
         "src/store/*": "state manager"
     }
-    "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://github.com/rabbitmu/vue-boilerplate'
+    "completeMessage": "Download Sucess"
 }
