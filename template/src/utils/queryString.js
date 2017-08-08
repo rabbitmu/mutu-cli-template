31aa6e712bfaa0e22d9d9b49ddf76f6e8d1dbb8a
muming <muming90@foxmail.com>
2017-08-08 14:45:48 +0800

    rewrite queryString api

--- template/src/utils/queryString.js
+++ template/src/utils/queryString.js
@@ -1,49 +1,13 @@
-/**
- * Query String 读写
- */
+export default function() {
+    let query = {}
+    
+    const searchString = location.search.slice(1) || location.hash.slice(location.hash.indexOf('?'))
 
-export class QueryString {
+    const reg = /&?(\w+)=([^&]+)(?:$|&)/igm
 
-    constructor(searchString) {
-        this._searchString = searchString
-        this._query = {}
-
-        let queryString = this._searchString.slice(1)
-        let reg = /&?(\w+)=([^&]+)(?:$|&)/igm
-
-        while(reg.test(queryString)) {
-            this._query[RegExp.$1] = RegExp.$2
-        }
+    while(reg.test(searchString)) {
+        query[RegExp.$1] = decodeURIComponent(RegExp.$2)
     }
 
-    get fullString() {
-        return this._searchString +'&'
-    }
-
-    getValue(key) {
-        return  decodeURIComponent(this._query[key]) || void 0
-    }
-
-    setValue(key, value) {
-        this._query[key] = encodeURIComponent(value)
-    }
-
-    serialize() {
-        let queryString = []
-
-        for (let key in this._query) {
-            if (this._query.hasOwnProperty(key)) {
-                queryString.push(key + '=' + this._query[key])
-            }
-        }
-
-        return queryString.join('&')
-    }
-
-    getLastPathname() {
-        let str = window.location.pathname
-        return str.substring(0, str.lastIndexOf('/') + 1)
-    }
+    return query
 }
-
-export default new QueryString(window.location.search)
